f958d843de911b44cf3d5e730bf02bb73de12382
SimonFrom <simon.bulder@gmail.com>
2025-03-10 12:21:40 +0100

    asd

--- WPFNavigation02/ViewModels/AddPersonViewModel.cs
+++ WPFNavigation02/ViewModels/AddPersonViewModel.cs
@@ -7,24 +7,56 @@ using System.Windows.Input;
 using WPFNavigation02.Stores;
 using WPFNavigation02.Commands;
 using WPFNavigation02.Services;
+using WPFNavigation02.Models;
 
 namespace WPFNavigation02.ViewModels
 {
     public class AddPersonViewModel : BaseViewModel
     {
         private readonly NavigationStore navigationStore;
-        private readonly PersonService personService;
+        private readonly PersonService _personService;
         public ICommand NavigatePersonListViewCommand { get; }
+        public ICommand AddPersonCommand { get; }
 
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public int Age { get; set; }
-
-        public AddPersonViewModel(NavigationStore navigationStore, PersonService personService) 
+        private string _firstName;
+        public string FirstName
         {
-            this.navigationStore = navigationStore;
-            this.personService = personService;
-            NavigatePersonListViewCommand = new NavigateCommand(new NavigationService(navigationStore, () => new PersonListViewModel(navigationStore, personService)));
+            get => _firstName;
+            set { _firstName = value; OnPropertyChanged(nameof(FirstName)); }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get => _lastName;
+            set { _lastName = value; OnPropertyChanged(nameof(LastName)); }
+        }
+
+        private int _age;
+        public int Age
+        {
+            get => _age;
+            set { _age = value; OnPropertyChanged(nameof(Age)); }
+        }
+
+
+        public AddPersonViewModel(NavigationStore navigationStore, PersonService personService)
+        {
+            _personService = personService;
+
+            AddPersonCommand = new NavigateCommand(new NavigationService(navigationStore, () =>
+            {
+
+                Person newPerson;
+                newPerson = new Person(FirstName, LastName, Age);
+                _personService.Add(newPerson);
+
+
+                return new PersonListViewModel(navigationStore, _personService);
+            }));
+
+
         }
     }
 }
+
